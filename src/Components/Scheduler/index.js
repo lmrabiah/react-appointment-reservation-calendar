e3cc5e39c832d5aae05ccf22dbc276535bba4672
Yusuf Abayomi <Yomi@users-MacBook-Pro.local>
2019-10-16 16:10:56 +0100

    Added comments to the scheduler component

--- src/Components/Scheduler/index.js
+++ src/Components/Scheduler/index.js
@@ -1,59 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Calendar from "../Calendar";
-import moment from 'moment';
+import moment from "moment";
 import { getWeekDays } from "../../Helpers";
-import CalendarControls from '../CalendarControls';
-import { 
-    Container
-} from 'reactstrap';
+import CalendarControls from "../CalendarControls";
+import { Container } from "reactstrap";
 
 const Schedular = () => {
+  const [weekCounter, setWeekCounter] = useState(0);
+  const [weekDays, setWeekDays] = useState(
+    getWeekDays(moment().add(weekCounter, "weeks"))
+  );
+  const [startDate, setStartDate] = useState(moment());
 
-    const [weekCounter, setWeekCounter] = useState(0)
-    const [weekDays, setWeekDays] = useState(getWeekDays(moment().add(weekCounter, 'weeks')))
-    const [startDate, setStartDate] = useState(moment());
+  // the event handler for the NEXT and PREVIOUS week button
+  const handlePrevNextWeekButtons = type => {
+    const newWeekCounter = type === "next" ? weekCounter + 1 : weekCounter - 1;
 
-    const handlePrevNextWeekButtons = type => {
-        const newWeekCounter = (type === 'next') ? weekCounter+1 : weekCounter-1;
+    const newWeek = moment().add(newWeekCounter, "weeks");
+    setWeekCounter(newWeekCounter);
+    setWeekDays(getWeekDays(newWeek));
+    setStartDate(newWeek);
+  };
 
-        const newWeek = moment().add(newWeekCounter, 'weeks');
-        setWeekCounter(newWeekCounter)
-        setWeekDays(getWeekDays(newWeek))
-        setStartDate(newWeek);
-    }
+  // changes the calender week to the week selected on the date picker
+  const handleDatePickerChange = datePickerValue => {
+    const currentDate = moment().add(weekCounter, "weeks");
+    const selectedDate = moment(datePickerValue);
+    const difference = selectedDate.diff(currentDate, "weeks");
+    const newWeekCounter = weekCounter + difference;
 
-    const handleDatePickerChange = datePickerValue => {
+    const newWeek = moment().add(newWeekCounter, "weeks");
+    setWeekCounter(newWeekCounter);
+    setWeekDays(getWeekDays(newWeek));
+    setStartDate(newWeek);
+  };
 
-        const currentDate = moment().add(weekCounter, 'weeks');
-        const selectedDate = moment(datePickerValue);
-        const difference = selectedDate.diff(currentDate, 'weeks');
-        const newWeekCounter = weekCounter + difference;
+  // determines if the timeslot is booked, past or available to be booked
+  const timeSlotStatus = (slotDate, slotTime) => {
+    const slotDateTime = moment(slotDate).add(slotTime, "hour");
+    if (moment().diff(slotDateTime) >= 0) return "past";
+    else return "present";
+  };
 
-        const newWeek = moment().add(newWeekCounter, 'weeks')
-        setWeekCounter(newWeekCounter)
-        setWeekDays(getWeekDays(newWeek))
-        setStartDate(newWeek)
-    }
-
-    const timeSlotStatus = (slotDate, slotTime) => {
-        const  slotDateTime =  moment(slotDate).add(slotTime, 'hour')
-        if(moment().diff(slotDateTime) >= 0) return "past"
-        else return "present";
-    }
-
-    return (
-        <Container>
-            <CalendarControls
-            handleDatePickerChange={handleDatePickerChange} 
-            handleClick={handlePrevNextWeekButtons}
-            weekdays={weekDays}
-            startDate={startDate} />
-            <Calendar 
-                weekdays={weekDays}
-                slotStatus={timeSlotStatus} />
-        </Container>
-        
-    )
-}
+  return (
+    <Container>
+      <CalendarControls
+        handleDatePickerChange={handleDatePickerChange}
+        handleClick={handlePrevNextWeekButtons}
+        weekdays={weekDays}
+        startDate={startDate}
+      />
+      <Calendar weekdays={weekDays} slotStatus={timeSlotStatus} />
+    </Container>
+  );
+};
 
 export default Schedular;
